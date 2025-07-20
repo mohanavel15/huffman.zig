4920768c73dee9132a5ef576e773b96a6437a991
Mohan <mohanavel15@protonmail.com>
2025-07-20 19:16:06 +0530

    compress & decompress files

--- main.zig
+++ main.zig
@@ -1,45 +1,76 @@
 const huffman = @import("huffman.zig");
 const std = @import("std");
 
-pub fn main() !void {
-    const message =
-        \\ In computer science and information theory,
-        \\ a Huffman code is a particular type of optimal
-        \\ prefix code that is commonly used for lossless
-        \\ data compression. The process of finding or using
-        \\ such a code is Huffman coding, an algorithm
-        \\ developed by David A. Huffman while he was a
-        \\ Sc.D. student at MIT, and published in the 1952 paper
-        \\ "A Method for the Construction of Minimum-Redundancy Codes"
-    ;
+const Allocator = std.mem.Allocator;
+const process = std.process;
 
+pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
         const leaked = gpa.deinit();
         if (leaked == .leak) {
-            @panic("Memory Leaked!");
-        } else {
-            std.log.info("Cleaned Memory Successfully", .{});
+            @panic("Memory Leaked in Compress File!");
         }
     }
 
-    var allocator = gpa.allocator();
+    const allocator = gpa.allocator();
 
-    var encoded = try huffman.encode(allocator, @constCast(message));
+    const args = try process.argsAlloc(allocator);
+    defer process.argsFree(allocator, args);
+
+    if (args.len < 3) {
+        std.debug.print("Invalid Arguments\n", .{});
+        return;
+    }
+
+    const mode = args[1];
+    const filename = args[2];
+
+    if (std.mem.eql(u8, mode, "-c")) {
+        try compress_file(allocator, filename);
+    } else if (std.mem.eql(u8, mode, "-d")) {
+        try decompress_file(allocator, filename);
+    } else {
+        std.debug.print("Invalid Arguments", .{});
+    }
+}
+
+fn compress_file(allocator: Allocator, filename: []const u8) !void {
+    const original_file = try std.fs.cwd().openFile(filename, .{});
+    defer original_file.close();
+
+    const buffer = try original_file.readToEndAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(buffer);
+
+    var encoded = try huffman.encode(allocator, buffer);
     defer encoded.deinit(allocator);
+
+    const filename_compress = std.fmt.allocPrint(allocator, "{s}.huffman", .{filename}) catch unreachable;
+    defer allocator.free(filename_compress);
+
+    const compressed_file = try std.fs.cwd().createFile(filename_compress, .{});
+    defer compressed_file.close();
+
+    const final_buf = try encoded.serialize(allocator);
+    defer allocator.free(final_buf);
+
+    try compressed_file.writeAll(final_buf);
+}
+
+pub fn decompress_file(allocator: Allocator, filename: []const u8) !void {
+    const file = try std.fs.cwd().openFile(filename, .{});
+    defer file.close();
+
+    const buffer = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(buffer);
+
+    var encoded = huffman.Encoded.deserialize(buffer);
 
     const decoded = try huffman.decode(allocator, &encoded);
     defer allocator.free(decoded);
 
-    std.debug.print("----Raw----\n", .{});
-    std.debug.print("Length: {}\n", .{message.len});
-    std.debug.print("Bits: {}\n", .{message.len * 8});
+    const decoded_file = try std.fs.cwd().createFile(filename[0 .. filename.len - 8], .{});
+    defer decoded_file.close();
 
-    std.debug.print("----Encoded----\n", .{});
-    std.debug.print("Length: {}\n", .{encoded.compressed_length});
-    std.debug.print("Bits: {}\n", .{encoded.bit_length});
-
-    std.debug.print("----Decoded----\n", .{});
-    std.debug.print("Lossless {}\n", .{std.mem.eql(u8, @constCast(message), decoded)});
-    std.debug.print("{s}\n", .{decoded});
+    try decoded_file.writeAll(decoded);
 }
